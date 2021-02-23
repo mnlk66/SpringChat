b9d23665be9f1b6a7e2522b1cb345381408bac74
mnlk <menelik-971@hotmail.com>
2021-02-23 17:50:29 -0400

    new project CHat APP SPRING

---
+++ src/test/java/com/web_service/springchat/SpringchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_service.springchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
